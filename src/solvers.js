4d00af59caca8509e8d76532e1755df6a9ece4de
jsmithb117 <jsmithb117@gmail.com>
2020-11-14 11:57:58 -0800

    Complete Advanced Content

--- src/solvers.js
+++ src/solvers.js
@@ -14,9 +14,7 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
 window.findNRooksSolution = function(n) {
-
   var matrix = window.makeEmptyMatrix(n);
   var newBoard = new Board(matrix);
   var matrixInBoard = newBoard.rows();
@@ -30,7 +28,6 @@ window.findNRooksSolution = function(n) {
       }
     }
   }
-  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(matrixInBoard));
   return matrixInBoard;
 };
 
@@ -43,67 +40,57 @@ window.countNRooksSolutions = function(n) {
   var boards = [];
   var breaking = false;
 
-  //function
   var generateSolutions = function(row) {
-    if (breaking === true) { return; }
-    if ( row < 0 ) { return; }
     if (row === n) {
-      console.log ('right here -> ', JSON.stringify(matrixInBoard));
       solutions++;
     } else {
-      for ( var col = 0; col < n; col++) {
+      if (n % 2 === 0 && row === 0 && n > 2) {
+        if (n === 2) {
+          var m = n / 2 + 1;
+        } else {
+          var m = n / 2;
+        }
+      } else {
+        var m = n;
+      }
+      for ( var col = 0; col < m; col++) {
         matrixInBoard[row][col] = 1;
         if (!newBoard.hasAnyRooksConflicts()) {
           generateSolutions(row + 1);
           if ( breaking === true ) { break; }
-
           matrixInBoard[row][col] = 0;
         } else {
-          if ( n > 1 && n % 2 === 0) {
-            if (matrixInBoard[0][Math.floor(n / 2)] === 1 && matrixInBoard[1][n / 2 - 1] === 1) {
-            // n is even
-            //  console.log('middle -> ', JSON.stringify(matrixInBoard));
-            // console.log('solutions = ', solutions);
-              //solutions * 2;
-              //solutions + 8;
-              //generateSolutions(-1);
-              // return;
-            }
-          } else if ( n > 2 && n % 2 === 1) {
+          if ( n > 2 && n % 2 === 1) {
             if (matrixInBoard[0][Math.floor(n / 2)] === 1 && matrixInBoard[1][Math.floor(n / 2)] === 1) {
-
               solutions *= 2;
               breaking = true;
-              if ( breaking === true ) { break; }
+              break;
             }
           }
           matrixInBoard[row][col] = 0;
         }
       }
     }
-    // console.log('solutions -> ', matrixInBoard);
     return;
   };
   generateSolutions(0);
-  //console.log('Number of solutions for ' + n + ' rooks:', solutions);
 
-  return solutions;
+  if (n % 2 === 0 && n > 2) {
+    return solutions * 2;
+  } else {
+    return solutions;
+  }
 };
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-
-
   var matrix = window.makeEmptyMatrix(n);
   var newBoard = new Board(matrix);
   var matrixInBoard = newBoard.rows();
-
   var queens = 0;
 
   if (n === 0) { return []; }
   if (n === 1) { return [[1]]; }
   if (n === 2 || n === 3) {
-    // debugger;
     return matrixInBoard;
   }
   var generateSolutions = function(row) {
@@ -112,8 +99,6 @@ window.findNQueensSolution = function(n) {
     } else {
       for ( var col = 0; col < n; col++) {
         matrixInBoard[row][col] = 1;
-
-
         queens++;
         if (!newBoard.hasAnyQueensConflicts()) {
           generateSolutions(row + 1);
@@ -130,10 +115,8 @@ window.findNQueensSolution = function(n) {
     }
     return;
   };
-  // console.log('Number of solutions for ' + n + ' queens:', solutions);
-  generateSolutions(0);
-  return generateSolutions(0);
 
+  return generateSolutions(0);
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
@@ -145,13 +128,35 @@ window.countNQueensSolutions = function(n) {
   var boards = [];
 
   var generateSolutions = function(row) {
+    if (n === 0 || n === 1) {
+      solutions += 1;
+      return;
+    }
     if ( row === n) {
       solutions++;
     } else {
-      for ( var col = 0; col < n; col++) {
+      if (n % 2 === 0 && row === 0) {
+        var m = n / 2;
+      } else {
+        var m = n;
+      }
+      for ( var col = 0; col < m; col++) {
         matrixInBoard[row][col] = 1;
+        if (n % 2 === 1) {
+          var first = matrixInBoard[0][Math.floor(n / 2)] === 1;
+          var second = matrixInBoard[1][Math.floor(n / 2)] === 1;
+          if (first && second) {
+            solutions *= 2;
+            return;
+          }
+        }
         if (!newBoard.hasAnyQueensConflicts()) {
           generateSolutions(row + 1);
+          var first = matrixInBoard[0][Math.floor(n / 2)] === 1;
+          var second = matrixInBoard[1][Math.floor(n / 2)] === 1;
+          if (first && second) {
+            return;
+          }
           matrixInBoard[row][col] = 0;
         } else {
           matrixInBoard[row][col] = 0;
@@ -160,7 +165,10 @@ window.countNQueensSolutions = function(n) {
     }
     return;
   };
-  console.log('Number of solutions for ' + n + ' queens:', solutions);
   generateSolutions(0);
-  return solutions;
+  if (n % 2 === 0 && n !== 0) {
+    return solutions * 2;
+  } else {
+    return solutions;
+  }
 };
